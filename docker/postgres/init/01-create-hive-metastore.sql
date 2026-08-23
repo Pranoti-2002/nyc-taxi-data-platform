5cbc875837eff32b802403ab8d4d7540a10ac6c3
horizonchaser12 <suryakant.mangaraj@gmail.com>
2026-08-23 16:26:22 +0530

    defaulted hue metastore to postgres from sqllite

--- docker/postgres/init/01-create-hive-metastore.sql
+++ docker/postgres/init/01-create-hive-metastore.sql
@@ -1,3 +1,20 @@
+-- ==========================================================
+-- Hive Metastore
+-- ==========================================================
+
 CREATE USER hiveuser WITH PASSWORD 'hivepassword';
+
 CREATE DATABASE metastore OWNER hiveuser;
+
 GRANT ALL PRIVILEGES ON DATABASE metastore TO hiveuser;
+
+
+-- ==========================================================
+-- Hue
+-- ==========================================================
+
+CREATE USER hueuser WITH PASSWORD 'huepassword';
+
+CREATE DATABASE hue OWNER hueuser;
+
+GRANT ALL PRIVILEGES ON DATABASE hue TO hueuser;
